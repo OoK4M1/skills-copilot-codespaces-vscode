8fb158e491363dde9fe8f0b8397b22d7e6403967
OoK4M1 <woodyjs2503@hotmail.com>
2023-11-30 22:16:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,3 @@
-// create web server just with javascript
-// run: node comments.js
-// open browser and go to http://localhost:3000
-// stop server: ctrl + c
+// Create web server 
+
+// Import modules
