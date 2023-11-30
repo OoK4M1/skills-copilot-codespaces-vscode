68a25d8786b7a2ad9a00f8b552470e01aa240d26
OoK4M1 <woodyjs2503@hotmail.com>
2023-11-30 22:09:45 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,41 @@
-// create web server that listens for requests
-// to the path /comments.json, and responds with
-// a JSON string of hard-coded comments
-var http = require('http');
-var comments = require('./comments');
+// create web server with express
+const express = require('express');
+const app = express();
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(comments));
+// create route handler for get request
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id]);
 });
 
-var port = 8080;
-server.listen(port);
+// listen on port 4001
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
 
-console.log("server listening on port " + port);
+// Comments
+const commentsByPostId = {
+  1: [
+    { id: 1, content: 'Comment 1' },
+    { id: 2, content: 'Comment 2' },
+    { id: 3, content: 'Comment 3' },
+  ],
+  2: [
+    { id: 4, content: 'Comment 4' },
+    { id: 5, content: 'Comment 5' },
+    { id: 6, content: 'Comment 6' },
+  ],
+  3: [
+    { id: 7, content: 'Comment 7' },
+    { id: 8, content: 'Comment 8' },
+    { id: 9, content: 'Comment 9' },
+  ],
+  4: [
+    { id: 10, content: 'Comment 10' },
+    { id: 11, content: 'Comment 11' },
+    { id: 12, content: 'Comment 12' },
+  ],
+};
+```
+
+###
 
